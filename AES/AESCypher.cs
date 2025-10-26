ce0fcbec7bf11e79a4c98376cd8799018253335c
dramlian <jankovdamian@gmail.com>
2025-10-26 15:42:14 +0100

    Modularized AES implementation for a cypher done

---
+++ AES/AESCypher.cs
@@ -0,0 +1,38 @@
+using AES;
+
+public class AESCypher
+{
+    private readonly AddRoundKey _addRoundKey;
+    private readonly SubBytes _subBytes;
+    private readonly ShiftRows _shiftRows;
+    private readonly MixColumns _mixColumns;
+    private readonly KeySchedule _keySchedule;
+
+    public AESCypher(AddRoundKey addRoundKey, SubBytes subBytes, ShiftRows shiftRows, MixColumns mixColumns, KeySchedule keySchedule)
+    {
+        _addRoundKey = addRoundKey;
+        _subBytes = subBytes;
+        _shiftRows = shiftRows;
+        _mixColumns = mixColumns;
+        _keySchedule = keySchedule;
+    }
+
+    public void Encrypt(byte[] input, byte[] key)
+    {
+        _addRoundKey.AddRoundKeyTransform(input, key);
+
+        for (int round = 0; round < 10; round++)
+        {
+            byte[] transformedInput = _subBytes.SubBytesTransform(input);
+            Array.Copy(transformedInput, input, transformedInput.Length);
+            _shiftRows.ShiftRowsTransform(input);
+            if (round != 9)
+            {
+                _mixColumns.MixColumnsTransform(input);
+            }
+            key = _keySchedule.CreateRoundKey(key, round);
+            _addRoundKey.AddRoundKeyTransform(input, key);
+        }
+    }
+
+}
